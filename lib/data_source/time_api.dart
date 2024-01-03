fb5f04d48c513341f713cddd2bdfdcbf8c7df2bb
Kimdahee <dh980502@gmail.com>
2024-01-04 02:26:22 +0900

    Future 이용한 원래방식

--- lib/data_source/time_api.dart
+++ lib/data_source/time_api.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:time_app/model/time.dart';
 
 class TimeApi {
-  Future<Time> getTimeResult() async {
+  Future<Time> getTime() async {
     final response = await http
         .get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Seoul'));
     return Time.fromJson(jsonDecode(response.body));
--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:time_app/data_source/time_api.dart';
+import 'package:time_app/model/time.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Time? _time;
+  final _api = TimeApi();
+
+  bool get isLoading => _time == null;
+
+  Future<void> init() async {
+    final time = await _api.getTime();
+    setState(() {
+      _time = time;
+    });
+  }
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +34,27 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('원래방식'),
       ),
-
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _time!.time,
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    _time!.utcTime,
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
