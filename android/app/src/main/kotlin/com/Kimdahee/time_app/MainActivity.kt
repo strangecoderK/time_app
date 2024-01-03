22993b6a82d7fd521fd49212c3e794ad472c8eee
Kimdahee <dh980502@gmail.com>
2024-01-04 01:44:57 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/Kimdahee/time_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Kimdahee.time_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data_source/time_api.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:time_app/model/time.dart';
+
+class TimeApi {
+  Future<Time> getTimeResult() async {
+    final response = await http
+        .get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Seoul'));
+    return Time.fromJson(jsonDecode(response.body));
+  }
+}
